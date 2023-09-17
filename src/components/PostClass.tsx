55631452b4df8e6b9fac34cbd88c197a4666e966
GavinXZhang <119836978+GavinXZhang@users.noreply.github.com>
2023-09-17 01:29:03 -0400

    Added Comments

--- src/components/PostClass.tsx
+++ src/components/PostClass.tsx
@@ -8,14 +8,14 @@ interface Props {
     updateStudentsData: (data: IGrade[]) => void; 
     classList: IUniversityClass[];
   }
-
+//creates this interface to make sure getlistGrades to return this type. 
   interface IStudentGrade {
     classId: string;
     grades: Record<string, number>;
     name: string;
     studentId: string;
 }
-
+//This section is for me to use Axios to get my APIs 
 const OnlySemester :string = "fall2022"
 const getlistGrades = async (studentId: string, classId: string): Promise<IStudentGrade> => {
     try{
@@ -82,7 +82,7 @@ const getListStudents = async (classId: string) => {
     }
 }
 export const PostComponent: React.FC<Props>= ({classList,updateClassList,updateStudentsData}) => {
-
+//executing the API to get the list of classes from the Fall2022 semester.
 useEffect(() => {
     const fetchData = async () => {
         const classes = await getClassesBySemester(OnlySemester);
@@ -91,24 +91,23 @@ useEffect(() => {
 
     fetchData();
 }, [updateClassList]);
-
+//this is where I start to build my data for each student insice of the classes
 useEffect(() => {
     const fetchAllStudentsForClasses = async (classList: IUniversityClass[]) => {
         const fetchedStudents: IGrade[] = await Promise.all(
+            //using the new classList I got from above, call the about API functions and feed them the right parameters to fill out the rest student data.
             classList.map(async (classItem): Promise<IGrade[]> => {
                 const studentIDs = await getListStudents(classItem.classId);
                 const studentNames = await Promise.all(studentIDs.map((id: string) => getStudentById(id)));
+                //Using the student Ids from studentIDs (calling to get a list of the students in the class) to get student's name
                 const assignmentWeight = await getListAssignments(classItem.classId); 
-                
+                //Here starts to build a profile for each student
                 const studentGrades = await Promise.all(studentIDs.map(async (id: string, index: number): Promise<IGrade> => {
                     const gradesObject: IStudentGrade = await getlistGrades(id, classItem.classId); 
-                    const gradesValues = Object.values(gradesObject.grades);
+                    const gradesValues = Object.values(gradesObject.grades); //ignores the A1, A2, A3 etc
                     const gradesOfFirstStudent = gradesValues[0]; // This gives you the object {A1: '97', A2: '92', ...}
-                    const numericalGrades = Object.values(gradesOfFirstStudent).map(Number); // Convert strings to numbers
-                    const totalGrade = numericalGrades.reduce((a, b) => a + b, 0) * assignmentWeight/100;
-                    console.log('gradesValues', gradesValues);
-                    console.log('assignmentWeight', assignmentWeight);
-                    console.log('totalGrade before formatting', totalGrade);
+                    const numericalGrades = Object.values(gradesOfFirstStudent).map(Number); // Convert strings to numbers 
+                    const totalGrade = numericalGrades.reduce((a, b) => a + b, 0) * assignmentWeight/100; //Redue combines the value
                     return {
                         StudentID: id,
                         StudentName: studentNames[index] || '',
